44ad92f6dc349736ba39c6664bba4f1d3254d066
Eric <eriqthedev@gmail.com>
2023-02-06 11:39:19 +0300

    user From and display board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DisplayBoardComponent } from './display-board/display-board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DisplayBoardComponent
   ],
   imports: [
     BrowserModule,
